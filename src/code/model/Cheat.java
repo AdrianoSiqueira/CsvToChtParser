b36bccf7d12d031a26ae1995d64d87b15abadf15
AdrianoSiqueira <as.adriano.siqueira@gmail.com>
2021-11-18 19:49:23 -0300

    Model class of a cheat.

---
+++ src/code/model/Cheat.java
@@ -0,0 +1,67 @@
+package code.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * <p>Model an individual cheat. It has the attributes conforming to the data
+ * from the CSV file.</p>
+ *
+ * @author Adriano Siqueira
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+public class Cheat {
+
+    private String       group;
+    private String       type;
+    private String       activation;
+    private String       description;
+    private List<String> codes;
+
+
+    public Cheat() {
+        this.codes = new ArrayList<>();
+    }
+
+
+    public String getGroup() {
+        return group;
+    }
+
+    public void setGroup(String group) {
+        this.group = group;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getActivation() {
+        return activation;
+    }
+
+    public void setActivation(String activation) {
+        this.activation = activation;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<String> getCodes() {
+        return codes;
+    }
+
+    public void addCode(String code) {
+        codes.add(code);
+    }
+}
